2838d594f4a115fd485d543921ca9df5a96d81af
Zahar Kshyvak <141167562+anestxs@users.noreply.github.com>
2026-07-17 21:55:43 +0300

    feature: db client wiring

---
+++ packages/db/index.ts
@@ -0,0 +1,4 @@
+export { PrismaClient } from './generated/prisma/client';
+export { PrismaPg } from '@prisma/adapter-pg';
+export * from './generated/prisma/enums';
+export type * from './generated/prisma/models';
